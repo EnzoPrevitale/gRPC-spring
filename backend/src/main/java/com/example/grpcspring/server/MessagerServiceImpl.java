f7c78ca02f9723194791d3118960cd26c93c9b43
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2026-06-25 11:00:01 -0300

    feat: implement messager service

---
+++ backend/src/main/java/com/example/grpcspring/server/MessagerServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.grpcspring.server;
+
+import com.example.grpcspring.MessageRequest;
+import com.example.grpcspring.MessageResponse;
+import com.example.grpcspring.MessagerGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+public class MessagerServiceImpl extends MessagerGrpc.MessagerImplBase {
+
+    @Override
+    public StreamObserver<MessageRequest> sendMessage(StreamObserver<MessageResponse> responseObserver) {
+        return new StreamObserver<MessageRequest>() {
+            @Override
+            public void onNext(MessageRequest messageRequest) {
+                MessageResponse response = MessageResponse.newBuilder()
+                        .setMessage("Recebido: " + messageRequest.getMessage())
+                        .build();
+                responseObserver.onNext(response);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+    }
+}
